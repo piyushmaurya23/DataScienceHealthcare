35463c7efa006526826ae452972fbda1832d6dff
fahad-gcet <fms525@outlook.com>
2017-04-03 20:27:18 +0530

    add csv files

--- app/views.py
+++ app/views.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
 
 def list(request):
     # Handle file upload
